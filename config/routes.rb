6da8cf41e891ce36a09e0aae354848a1757e634d
Mera3412 <82443779+Mera3412@users.noreply.github.com>
2023-07-20 15:03:38 +0900

    rootの位置の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  
   devise_for :users
   resources :sample_articles
   resources :articles
-
-  root "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
