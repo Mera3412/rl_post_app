06f9b59238d9cc591a48172440e92a93c7281614
Mera3412 <82443779+Mera3412@users.noreply.github.com>
2023-07-21 11:11:38 +0900

    sample_articleの削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root "articles#index"
 
   devise_for :users
-  resources :sample_articles
   resources :articles
   resources :mypage, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
